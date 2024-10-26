f56c613f69c44e536650c9dc1cf6b9d9ad61e7d0
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-10-26 15:07:48 +0300

    Fixed serializers.py

--- plants/serializers.py
+++ plants/serializers.py
@@ -6,5 +6,17 @@ from plants.models import Plant
 class PlantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plant
-        fields = ("id", "name", "species", "watering_frequency_days", "last_watered_days",)
-        read_only_fields = ("id", "name", "species", "watering_frequency_days",)
+        fields = ("id", "name", "species", "watering_frequency_days", "last_watered_date",)
+        extra_kwargs = {
+            "name": {"required": True},
+            "species": {"required": True},
+            "watering_frequency_days": {"required": True},
+            "last_watered_date": {"required": False}
+        }
+
+
+class PlantDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plant
+        fields = ("id", "name", "species", "watering_frequency_days", "last_watered_date",)
+        read_only_fields = ("id", "name", "species", "watering_frequency_days")
