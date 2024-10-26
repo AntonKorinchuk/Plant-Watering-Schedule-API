2b11819818bd158102eef9099e56e78c3e0f9f71
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-10-26 14:22:42 +0300

    Add model Plant with migration

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    watering_frequency_days = models.PositiveIntegerField()
+    last_watered_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.species}"
